8e9b0949d104075fff76720677682012691e0d5b
M <muraliprasad.murthy@schindler.com>
2026-03-04 18:02:13 +0530

    Fix Terraform syntax error in module variables

--- infra/modules/tgw-vpn/variables.tf
+++ infra/modules/tgw-vpn/variables.tf
@@ -6,9 +6,12 @@ variable "domain"     { type = string }
 variable "vpc_id"         { type = string }
 variable "tgw_subnet_ids" { type = list(string) }
 
-variable "create_tgw"      { type = bool }
-variable "existing_tgw_id" { type = string default = null }
-variable "tgw_asn"         { type = number }
+variable "create_tgw" { type = bool }
+variable "existing_tgw_id" {
+  type    = string
+  default = null
+}
+variable "tgw_asn" { type = number }
 
 variable "cgw_public_ips" { type = list(string) }
 variable "cgw_bgp_asn"    { type = number }
@@ -19,6 +22,12 @@ variable "propagate_vpc_attachment"            { type = bool }
 variable "propagate_vpn_attachment"            { type = bool }
 
 variable "vpn_tunnel_inside_cidrs" { type = list(string) }
-variable "vpn_tunnel_psk"          { type = string default = null }
+variable "vpn_tunnel_psk" {
+  type    = string
+  default = null
+}
 
-variable "tags" { type = map(string) default = {} }
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
